020a976031a1ca4215425bd749ff1082b9da6f1a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-10-08 12:59:24 +0200

    Add contraint check staff

--- sqlserver/scripts/sakila/staff.sql
+++ sqlserver/scripts/sakila/staff.sql
@@ -1,3 +1,6 @@
+ALTER TABLE staff NOCHECK CONSTRAINT fk_staff_store;
+ALTER TABLE staff NOCHECK CONSTRAINT fk_staff_address;
+
 SET IDENTITY_INSERT staff ON
 
 Insert into staff
@@ -11,3 +14,7 @@ Values
  ('2','Jon','Stephens','4',NULL,'Jon.Stephens@sakilastaff.com','2','1','Jon','8cb2237d0679ca88db6464eac60da96345513964','2006-02-15T04:57:16.000')
 ;
 SET IDENTITY_INSERT staff OFF
+
+ALTER TABLE staff CHECK CONSTRAINT fk_staff_store;
+ALTER TABLE staff CHECK CONSTRAINT fk_staff_address;
+
