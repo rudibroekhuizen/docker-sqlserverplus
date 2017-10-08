1028d129d88bfd95fe262636cc5d4e1b33941b43
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-10-08 12:39:40 +0200

    Add Sakila testdb

---
+++ sqlserver/scripts/sakila/language.sql
@@ -0,0 +1,34 @@
+SET IDENTITY_INSERT language ON
+
+Insert into language
+ (language_id,name,last_update)
+Values
+('1','English','2006-02-15T05:02:19.000')
+;
+Insert into language
+ (language_id,name,last_update)
+Values
+('2','Italian','2006-02-15T05:02:19.000')
+;
+Insert into language
+ (language_id,name,last_update)
+Values
+('3','Japanese','2006-02-15T05:02:19.000')
+;
+Insert into language
+ (language_id,name,last_update)
+Values
+('4','Mandarin','2006-02-15T05:02:19.000')
+;
+Insert into language
+ (language_id,name,last_update)
+Values
+('5','French','2006-02-15T05:02:19.000')
+;
+Insert into language
+ (language_id,name,last_update)
+Values
+('6','German','2006-02-15T05:02:19.000')
+;
+SET IDENTITY_INSERT language OFF
+
---
+++ sqlserver/scripts/sakila/store.sql
@@ -0,0 +1,13 @@
+SET IDENTITY_INSERT store ON
+
+Insert into store
+ (store_id,manager_staff_id,address_id,last_update)
+Values
+('1','1','1','2006-02-15T04:57:12.000')
+;
+Insert into store
+ (store_id,manager_staff_id,address_id,last_update)
+Values
+('2','2','2','2006-02-15T04:57:12.000')
+;
+SET IDENTITY_INSERT store OFF
